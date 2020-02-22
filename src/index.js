94edbb104b80fb1bdfdb79508a98eddbfef0d326
Rodrigo E. Gimenez <rodrigog83@gmail.com>
2020-02-22 23:13:00 +0100

    Move middleware helpers to middlewares.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const middlewares = require('./middlewares');
 
 // Import router
 const routes = require('./routes');
@@ -34,21 +35,10 @@ app.use(bodyParser.json());
 app.use('/', routes);
 
 
-app.use((req, res, next) => {
-  const error = new Error(`Not found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-});
+app.use(middlewares.notFound);
 
 // eslint-disable-next-line no-unused-vars
-app.use((error, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? '' : error.stack,
-  });
-});
+app.use(middlewares.errorHandler);
 
 
 // Connect to database using new parser and new connection management
