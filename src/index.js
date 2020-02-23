724367d5449d6241bcbd9cdf3cfbd18013c1acb5
Rodrigo E. Gimenez <rodrigog83@gmail.com>
2020-02-23 16:57:34 +0100

    Use express' builtin body parser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Import dependencies
 const console = require('console');
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -24,12 +24,12 @@ app.use(cors({
 // We first instruct bodyParser to parse the data received
 
 // Use bodyParser to parse urlencoded data sent in the body
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+// app.use(bodyParser.urlencoded({
+//   extended: true,
+// }));
 
 // Use bodyParser to parse json data sent with Content-Type: application/json
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Now we add our routing, which is abstracted in a routes.js file
 app.use('/', routes);
